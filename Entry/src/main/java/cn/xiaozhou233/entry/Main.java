2b52225ea8b3c86c04ba9feb5be02f0a9c9558d1
筱周XiaoZhou <xiaozhou233@vip.qq.com>
2025-10-06 23:48:36 +0800

    init

---
+++ Entry/src/main/java/cn/xiaozhou233/entry/Main.java
@@ -0,0 +1,8 @@
+package cn.xiaozhou233.entry;
+
+public class Main {
+    // Entry method must be public static
+    public static void entryMethod() {
+        System.out.println("ENTRY METHOD INVOKED!!!");
+    }
+}
